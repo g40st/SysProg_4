3c49970a68d201caf7ffba42abc9875ae43abb27
Thomas Buck <xythobuz@xythobuz.de>
2014-10-27 21:50:02 +0100

    beautified client/main.c

--- client/main.c
+++ client/main.c
@@ -7,8 +7,6 @@
  * main.c: Hauptprogramm des Clients
  */
 
-#include "common/util.h"
-#include "gui/gui_interface.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
@@ -21,11 +19,14 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
+#include "common/util.h"
+#include "gui/gui_interface.h"
+
 void show_help() {
     printf("Available options:\n");
-	printf("        -s --host       specify a host(argument) [required] \n");
-    printf("        -p --port       specify a port(argument) \n");
-    printf("        -h --help       show this help message \n");
+	printf("    -s --host    specify a host (argument)\n");
+    printf("    -p --port    specify a port (argument)\n");
+    printf("    -h --help    show this help message\n");
 }
 
 int main(int argc, char **argv) {
@@ -35,7 +36,7 @@ int main(int argc, char **argv) {
 	guiInit(&argc, &argv);
 	infoPrint("Client Gruppe 01");
 
-	/* Initialisierung: Verbindungsaufbau, Threads starten usw... */
+	// Initialisierung: Verbindungsaufbau, Threads starten usw...
 	struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -43,53 +44,39 @@ int main(int argc, char **argv) {
 
     const char* short_options = "hp:s:";
     struct option long_options[] = {
-        {"help", no_argument, 0, 'h'},
-        {"port", required_argument, 0, 'p'},
-        {"host", required_argument, 0, 's'},
-        {NULL, 0, NULL, 0}
+        { "help", no_argument, 0, 'h' },
+        { "port", required_argument, 0, 'p' },
+        { "host", required_argument, 0, 's' },
+        { NULL, 0, NULL, 0 }
     };
 
     int option_index = 0;
-    int c;
-
-    int argument = 0;
-    for(int i = 0; i < argc; i++) {
-        if((strcmp(argv[i], "-s") == 0) || (strcmp(argv[i], "--host")  == 0)) {
-            argument = 1;
-        }
-        else{
-        }
-    }
-    if(argument == 0) {
-        printf("host argument must be set!\n");
-        show_help();
-        exit(0);
-    }
-
-    while(1) {
-        c = getopt_long(argc, argv, short_options, long_options, &option_index);
-        if(c == -1) {
+    while (1) {
+        int c = getopt_long(argc, argv, short_options, long_options, &option_index);
+        if (c == -1) {
             break;
         }
-        switch(c) {
+
+        switch (c) {
             case 'h':
                 show_help();
-                exit(1);
-                break;
+                return 0;
+
             case 's':
-                if(optarg) {
+                if (optarg)
                     serv_addr.sin_addr.s_addr = inet_addr(optarg);
-                    break;
-                }
+                break;
+
             case 'p':
-                if(optarg) {
+                if (optarg)
                     serv_addr.sin_port = htons(atoi(optarg));
-                    break;
-                }
+                break;
+
             case '?':
-                    printf("Option not implemented yet -- %s (%c)\n", argv[optind-1], c);
-                    show_help();
-                    break;
+            default:
+                printf("Option not implemented yet -- %s (%c)\n", argv[optind-1], c);
+                show_help();
+                break;
         }
     }
 
@@ -106,7 +93,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-
     // Ausgabe/Eingabe
     printf("Serveradresse: %s\n", inet_ntoa(serv_addr.sin_addr));
     printf("Serverport: %i\n", ntohs(serv_addr.sin_port));
@@ -114,36 +100,30 @@ int main(int argc, char **argv) {
 	preparation_showWindow();
 	guiMain();
 
-	/* Resourcen freigeben usw... */
+	// Resourcen freigeben usw...
 	guiDestroy();
 
 	return 0;
 }
 
-void preparation_onCatalogChanged(const char *newSelection)
-{
+void preparation_onCatalogChanged(const char *newSelection) {
 	debugPrint("Katalogauswahl: %s", newSelection);
 }
 
-void preparation_onStartClicked(const char *currentSelection)
-{
+void preparation_onStartClicked(const char *currentSelection) {
 	debugPrint("Starte Katalog %s", currentSelection);
 }
 
-void preparation_onWindowClosed(void)
-{
+void preparation_onWindowClosed(void) {
 	debugPrint("Vorbereitungsfenster geschlossen");
 	guiQuit();
 }
 
-void game_onSubmitClicked(unsigned char selectedAnswers)
-{
-	debugPrint("Absende-Button angeklickt, Bitmaske der Antworten: %u",
-			(unsigned)selectedAnswers);
+void game_onSubmitClicked(unsigned char selectedAnswers) {
+	debugPrint("Absende-Button angeklickt, Bitmaske der Antworten: %u", (unsigned)selectedAnswers);
 }
 
-void game_onWindowClosed(void)
-{
+void game_onWindowClosed(void) {
 	debugPrint("Spielfenster geschlossen");
 	guiQuit();
 }
